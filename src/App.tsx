eb600b58cc80f4c00926148ab8c972be164d068f
FelipePulcherio <felipe.pulcherio1@gmail.com>
2024-11-27 22:20:18 -0800

    Add routes and call context

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
+import useAuth from './hooks/useAuth';
 import LandingPage from './pages/landingPage';
 import GamePage from './pages/gamePage';
-import PlayerPage from './pages/playerPage';
 
 export default function App() {
+    // Call contexts
+    const { isLoggedIn } = useAuth(); // Access the logged-in state from context
+
   return (
     <>
       <Routes>
         <Route path='/' element={<LandingPage />} />
-
-        {/* Conditional routes */}
-        <Route path='player' element={<PlayerPage />} />
-        <Route path='game' element={<GamePage />} />
+        <Route
+          path='game'
+          element={isLoggedIn ? <GamePage /> : <Navigate to='/' />}
+        />
         {/* Non-existing routes */}
         <Route path='/*' element={<Navigate to='/' />} />
       </Routes>
