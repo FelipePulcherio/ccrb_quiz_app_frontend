180cb3b755e5a58b7bc9db5945c2517652ae0f8c
FelipePulcherio <felipe.pulcherio1@gmail.com>
2024-11-27 22:20:46 -0800

    Import and use landingPage component

--- src/pages/landingPage.tsx
+++ src/pages/landingPage.tsx
@@ -1,19 +1,10 @@
-import axios from "axios";
+import LoginModal from "../components/loginModal";
 
 export default function LandingPage(): JSX.Element {
-  function FetchTest() {
-    axios.get('http://127.0.0.1:3000/users')
-      .then(response => {
-        console.log(response.data);
-      }, error => {
-        console.log(error);
-      });
-  }
-
   return (
     <>
       <div>LANDING PAGE</div>
-      <button onClick={() => FetchTest()} className="border-2 border-[#000000]">Fetch</button>
+      <LoginModal />
     </>
   );
 }
