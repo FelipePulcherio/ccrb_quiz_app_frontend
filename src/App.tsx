d8cca5aafbd694c5ec19cd3a8fa31977069f4973
FelipePulcherio <felipe.pulcherio1@gmail.com>
2024-11-30 17:41:39 -0800

    Update routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import LandingPage from './pages/landingPage';
 import GamePage from './pages/gamePage';
-import ResultsPage from './pages/ResultsPage';
-import PlayerPage from './pages/playerPage';
 
 export default function App() {
+    // Call contexts
+    const isLoggedIn = true; // Access the logged-in state from context
+
   return (
     <>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="player" element={<PlayerPage />} />
-        <Route path="game" element={<GamePage />} />
-        <Route path="results" element={<ResultsPage />} />
-        <Route path="/*" element={<Navigate to="/" />} />
+        <Route path='/' element={<LandingPage />} />
+        <Route
+          path='game'
+          element={isLoggedIn ? <GamePage /> : <Navigate to='/' />}
+        />
+        {/* Non-existing routes */}
+        <Route path='/*' element={<Navigate to='/' />} />
       </Routes>
     </>
   );
